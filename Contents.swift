20e4998ad1c8162e897ec4a11c68ee4b74291a4b
Markovic11 <fredericguallar@iMac-de-Frederic.local>
2015-12-02 21:17:06 +0100

    minireto
    minireto

---
+++ Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+for i in 0...100 {
+    
+    if i % 5 == 0{
+        print("#\(i)\tBingo!!!")
+        
+    }
+    if i % 2 == 0{
+        print("#\(i)\tPar!!!")
+        
+    }else{
+        print("#\(i)\timpar!!!")
+        
+        
+        
+    }
+    
+    if i >= 30 && i <= 40{
+        print("#\(i)\tVivaSwift!!!")
+        
+        
+    }
+    
+}
+
+
